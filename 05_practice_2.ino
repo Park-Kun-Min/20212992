aba71f4d98463c416a6d6f4fc064288aefda4f56
Park-Kun-Min <85788560+Park-Kun-Min@users.noreply.github.com>
2021-09-27 19:03:36 +0900

    Create 05_practice_2.ino

---
+++ 05_practice_2.ino
@@ -0,0 +1,17 @@
+void setup(){
+    pinMode(7, OUTPUT);
+}
+ 
+void loop(){
+  digitalWrite(7, LOW);
+  delay(1000);
+  for (int i=0; i < 6; i++) {
+    digitalWrite(7, LOW);
+    delay(100);
+    digitalWrite(7, HIGH);
+    delay(100);
+  }
+  while (true) {
+    delay(1000);
+  }
+}
